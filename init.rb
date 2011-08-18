58e3409c5c3998bd3d9753ba0e96b51f431f59a5
Ivan Schneider <isc@massivebraingames.com>
2011-08-18 12:37:00 +0200

    add a warning and request user confirmation before proceeding

--- init.rb
+++ init.rb
@@ -1,19 +1,28 @@
 module Heroku::Command
   class Dbinsights < BaseWithApp
-    Help.group("DBInsights") do |group|
-      group.command "dbinsights",                      "enable db insights for the current app"
+    Help.group("DbInsights") do |group|
+      group.command "dbinsights", "enable db insights for the current app"
     end
     
     def initialize(*args)
       super
       @config_vars = heroku.config_vars(app)
       @dbinsights_url = @config_vars["DBINSIGHTS_URL"]
-      abort(" !   Please add the dbinsights addon first.") unless @dbinsights_url
+      abort " !   Please add the dbinsights addon first." unless @dbinsights_url
     end
     
     def index
-      RestClient::Resource.new(@dbinsights_url).put :account => {:db_url => @config_vars['DATABASE_URL']}
-      puts "Successfully enabled db insights for the current app."
+      puts "In order to create line charts and pie charts from your data,"
+      puts "Dbinsights will store the DATABASE_URL of your application"
+      puts "(removing the addon will delete that info from the DbInsights database)"
+      puts "Do you confirm the plugin execution ? (yes / no)"
+      answer = gets
+      if answer.chomp == 'yes'
+        RestClient::Resource.new(@dbinsights_url).put :account => {:db_url => @config_vars['DATABASE_URL']}
+        puts "Successfully enabled DbInsights for the current app."
+      else
+        puts "Plugin execution aborted."
+      end
     end
 
   end
