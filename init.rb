a9124bf7607824787106f8cd13558dc090efb9eb
Ivan Schneider <git@ivanschneider.fr>
2012-11-13 20:04:04 +0100

    fix deprecations

--- init.rb
+++ init.rb
@@ -1,30 +1,32 @@
-module Heroku::Command
-  class Dbinsights < BaseWithApp
-    Help.group("DbInsights") do |group|
-      group.command "dbinsights", "enable db insights for the current app"
-    end
-    
-    def initialize(*args)
-      super
+module Heroku::Command::Dbinsights < Heroku::Command::Base
+  Help.group("DbInsights") do |group|
+    group.command "dbinsights", "enable db insights for the current app"
+  end
+  
+  def initialize(*args)
+    super
+    if respond_to? 'api'
+      @config_vars = api.get_config_vars(app).body
+    else
       @config_vars = heroku.config_vars(app)
-      @dbinsights_url = @config_vars["DBINSIGHTS_URL"]
-      abort " !   Please add the dbinsights addon first." unless @dbinsights_url
     end
-    
-    def index
-      puts "In order to create line charts and pie charts from your data,"
-      puts "Dbinsights will store an encrypted version of the DATABASE_URL of your application"
-      puts "(removing the addon will delete that info from the DbInsights database)"
-      puts "Do you confirm the plugin execution ? (yes / no)"
-      print "> "
-      answer = STDIN.gets
-      if answer.chomp == 'yes'
-        RestClient::Resource.new(@dbinsights_url).put :account => {:db_url => @config_vars['DATABASE_URL']}
-        puts "Successfully enabled DbInsights for the current app."
-      else
-        puts "Plugin execution aborted."
-      end
+    @dbinsights_url = @config_vars["DBINSIGHTS_URL"]
+    abort " !   Please add the dbinsights addon first." unless @dbinsights_url
+    require 'rest_client'
+  end
+  
+  def index
+    puts "In order to create line charts and pie charts from your data,"
+    puts "Dbinsights will store an encrypted version of the DATABASE_URL of your application"
+    puts "(removing the addon will delete that info from the DbInsights database)"
+    puts "Do you confirm the plugin execution ? (yes / no)"
+    print "> "
+    answer = STDIN.gets
+    if answer.chomp == 'yes'
+      RestClient::Resource.new(@dbinsights_url).put :account => {:db_url => @config_vars['DATABASE_URL']}
+      puts "Successfully enabled DbInsights for the current app."
+    else
+      puts "Plugin execution aborted."
     end
-
   end
 end
